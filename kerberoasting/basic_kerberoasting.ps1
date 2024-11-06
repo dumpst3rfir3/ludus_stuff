fff94062984abd2f43cfc439a21b6b0c78510621
Dumpst3r Fir3 <alex@dumpsterfire.miami>
2024-11-06 09:03:39 -0500

    Fixed PS script

--- kerberoasting/basic_kerberoasting.ps1
+++ kerberoasting/basic_kerberoasting.ps1
@@ -1,4 +1,4 @@
 Install-WindowsFeature -Name "RSAT-AD-PowerShell" -IncludeAllSubFeature
 Install-WindowsFeature Web-Server -IncludeManagementTools
-Start-Process "setspn.exe" -ArgumentList "-a HTTP/DF-SRV01-win2019.ludus.domain ludus\victim"
-Start-Process "setspn.exe" -ArgumentList "-a HTTP/DF-SRV01-win2019.ludus.domain ludus\stabby"
+Start-Process "setspn.exe" -ArgumentList "-a HTTP/DF-SRV01-win2019.dumpster.fire dumpster\victim"
+Start-Process "setspn.exe" -ArgumentList "-a HTTP/DF-SRV01-win2019.dumpster.fire dumpster\stabby"
