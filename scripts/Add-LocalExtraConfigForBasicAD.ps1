6c5e022d672e10dea5714a2ff900d3584d96283b
Dumpst3r Fir3 <alex@dumpsterfire.miami>
2025-02-14 16:50:37 -0500

    Deleted Local config script

--- scripts/Add-LocalExtraConfigForBasicAD.ps1
+++
@@ -1,3 +0,0 @@
-Add-LocalGroupMember -Group "Remote Desktop Users" -Member "dumpster\Sales CA"
-Add-LocalGroupMember -Group "Remote Desktop Users" -Member "dumpster\Sales US"
-Add-LocalGroupMember -Group "Administrators" -Member "dumpster\IT"
